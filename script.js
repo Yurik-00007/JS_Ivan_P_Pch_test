b861a09c97f3a94ae0d8c8281f04aa65e5ca67c8
yurik-007 <yurik-007@mail.ru>
2022-01-16 08:28:01 +0300

    Initial commit
    013

---
+++ script.js
@@ -0,0 +1,62 @@
+"use strict";
+
+/*
+console.log('arr'+'- object');
+console.log(4+ '- object');
+console.log(4+ +'5');
+
+let incr = 10,
+    decr = 10;
+
+    // incr++;
+    // decr--;
+
+    // ++incr;
+    // --decr;
+
+    // console.log(incr++ +' '+ decr--);
+    console.log(++incr +' '+ --decr);
+
+    console.log(5%2);
+    // console.log (2*4 == 8);
+    console.log(2 * 4 === '8');
+
+    let isChecked = true;
+    isClosed = false;
+
+    console.log(isChecked && isChecked);
+    */
+
+    // if (4 == 4){
+    //     console.log('Ok!');
+    // }else {
+    //     console.log('Error');
+    // }
+
+    // const num = 50;
+    // if (num < 49){
+    //     console.log('Error');
+    // } else if(num>100){
+    //     console.log('меньше');
+    // }else{
+    //     console.log('Ok!');
+    // }
+
+    // (num == 50) ? console.log('Ok!') : console.log('Error');
+
+    const num = 50;
+
+    switch(num){
+        case(49) : 
+        console.log('Неверно');
+        break;
+        case(100) : 
+        console.log('Неверно');
+        break;
+        case(51) : 
+        console.log('В точку!');
+        break;
+        default : 
+        console.log('Не в этот раз');
+        break;
+    }

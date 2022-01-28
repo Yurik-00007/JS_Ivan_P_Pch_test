f3b5650a42ce5667265c39b207768e871ab80315
yurik-007 <yurik-007@mail.ru>
2022-01-28 13:59:11 +0300

    2

--- js/script.js
+++ js/script.js
@@ -5,3 +5,17 @@ console.log(box);
 
 const btns = document.getElementsByTagName('button');//[1];
 console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const heart = document.querySelectorAll('.heart');
+console.log (heart);
+
+heart.forEach(function(item){
+    console.log(item);
+});
+
+heart.forEach(item => {
+    console.log(item);
+});

566b96d0480fd1ff9863a3ab7f8f16f2933f46c9
yurik-007 <yurik-007@mail.ru>
2022-01-30 20:43:13 +0300

    finish

--- script.js
+++ script.js
@@ -1,50 +1,22 @@
-const btn = document.querySelectorAll('button'),
-    overlay= document.querySelector('.overlay'),
-    btns =document.querySelectorAll('button');
-console.log(btn);
-console.log(overlay);
+// console.log(document.body);
+// console.log(document.head);
+console.log(document.body.childNodes);
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+//  console.log(document.body.firstElementChild);
 
+// console.log(document.querySelector('#current').parentNode);
+// console.log(document.querySelector('#current').parentElement);
 
-btn[1].onclick = function(){
-    alert('Click');
-}; 
-
-btn[1].onclick = function(){
-    alert('Second Click');
-}; 
-
-// btn[2].addEventListener( 'mouseenter', (e)=>{
-//     console.log(e.target);
-//     e.target.remove();
-    //console.log('Hover');
-// });
-// btn[2].addEventListener( 'click', ()=>{
-//     alert('Second click');
-// });
-// let i=0;
-const deleteElement=(e)=>{
-    console.log(e.currentTarget);
-    console.log(e.type);
-// console.log(i++);
-// if(i===1){
-//     btn[2].removeEventListener( 'click', deleteElement);
-// }
-};
-
-btn[2].addEventListener( 'click', deleteElement);
-overlay.addEventListener( 'click', deleteElement);
-// btn[2].removeEventListener( 'click', deleteElement);
-
-const link = document.querySelector('a');
-console.log(link);
-
-link.addEventListener('click', (e) =>{
-    e.preventDefault();
-    console.log(e.target);
-});
-
-btns.forEach(item => {
-    item.addEventListener('click', deleteElement, {once:true});
-});
-    
-
+// console.log(document.querySelector('#current').parentNode.parentNode);
+// console.log(document.querySelector('[data-current="3"]'));
+// console.log(document.querySelector('[data-current="3"]').nextSibling); 
+// console.log(document.querySelector('[data-current="3"]').previousSibling); 
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling); 
+// console.log(document.querySelector('[data-current="3"]').previousElementSibling); 
+for (let node of document.body.childNodes) {
+    if (node.nodeName == '#text') {
+        continue;
+    }
+    console.log(node);
+}

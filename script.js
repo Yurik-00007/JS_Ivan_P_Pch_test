5c7c544201bd4c8b6bf8d220a93ead30a6615a38
yurik-007 <yurik-007@mail.ru>
2022-02-01 09:20:31 +0300

    mobile

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-// console.log(document.body);
-// console.log(document.head);
-console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
-// console.log(document.body.lastChild);
-//  console.log(document.body.firstElementChild);
+window.addEventListener('DOMContentLoaded',() =>{
+   const box=document.querySelector('.box'); 
+   box.addEventListener('touchstart', (e) =>{
+       e.preventDefault();
+       console.log('Start');
+       console.log(e.targetTouches);
+   });
 
-// console.log(document.querySelector('#current').parentNode);
-// console.log(document.querySelector('#current').parentElement);
+   box.addEventListener('touchmove', (e) =>{
+    e.preventDefault();
+    console.log('Move');
+    console.log(e.targetTouches[0].pageX);
+});
 
-// console.log(document.querySelector('#current').parentNode.parentNode);
-// console.log(document.querySelector('[data-current="3"]'));
-// console.log(document.querySelector('[data-current="3"]').nextSibling); 
-// console.log(document.querySelector('[data-current="3"]').previousSibling); 
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling); 
-// console.log(document.querySelector('[data-current="3"]').previousElementSibling); 
-for (let node of document.body.childNodes) {
-    if (node.nodeName == '#text') {
-        continue;
-    }
-    console.log(node);
-}
+box.addEventListener('touchend', (e) =>{
+    e.preventDefault();
+    console.log('End');
+});
+
+});

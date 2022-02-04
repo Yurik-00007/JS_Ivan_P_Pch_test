921f30ee469f75d2338b5613b146061df3691d27
yurik-007 <yurik-007@mail.ru>
2022-02-04 08:33:37 +0300

    037_classList, делегирование событий

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+/*
 const p = document.querySelectorAll('p');
 console.log(p);
 
@@ -10,3 +11,45 @@ document.body.append(script);
 
 loadScript('js/test.js');
 loadScript('js/some.js');
+*/
+const btns =  document.querySelectorAll('button'),
+wrapper=document.querySelector('.btn-block');
+// console.log(wrapper);
+// // // console.log(btns[0].classList.length);
+// // // console.log(btns[0]);
+// // // console.log(btns[0].classList.item(0));
+// // console.log(btns[1].classList.add('red','trrrrrrrttt'));
+// // // console.log(btns[0].classList.remove('blue'));
+// // console.log(btns[0].classList.toggle('blue'));
+
+// // if(btns[1].classList.contains('red')){
+// //     console.log('red');
+// // }
+
+// btns[0].addEventListener('click', () =>{
+//     // if(btns[1].classList.contains('red')){
+//     //     btns[1].classList.remove('red');
+//     // } else{
+//     //     btns[1].classList.add('red');
+//     // }
+//     btns[1].classList.toggle('red');
+// })
+// console.log(btns[0].className);
+
+
+// wrapper.addEventListener('click',(e)=> {
+//     // console.dir(e.target);
+//     if(e.target && e.target.tagName == 'BUTTON'){
+//         console.log('Hello');
+//     }
+// });
+
+btns.forEach(btn => {
+    btn.addEventListener('click', () =>{
+        console.log('Hello');
+    });
+});
+
+const btn=document.createElement ('button');
+btn.classList.add('red');
+wrapper.append(btn);

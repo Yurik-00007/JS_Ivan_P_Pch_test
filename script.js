af1bdeae534b3a55cde4739ad2b776e8bb231bd3
yurik-007 <yurik-007@mail.ru>
2022-02-11 07:42:01 +0300

    finish

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-'use strict';
+
+
+
 /*
 const p = document.querySelectorAll('p');
 console.log(p);
@@ -185,6 +187,7 @@ alert (`Цикл отработал за ${end-start} миллисекунд`);
 */
 
 //конструктор функция
+/*
 function User(name, id){
     this.name =name;
     this.id = id;
@@ -209,3 +212,83 @@ console.log(alex);
 
 ivan.exit();
 alex.exit();
+
+*/
+
+//контекст вызова функций
+// 'use strict';
+
+// function showThis(a,b){
+//     console.log(this);
+//     function sum(){
+//         console.log(this);
+//         return a+b;
+//     }
+
+//     console.log(sum());
+// }
+
+// showThis(4,5);
+
+// const obj = {
+//     a:20,
+//     b:15,
+//     sum :function(){
+//         function shout(){
+//         console.log(this);
+//         }
+//         shout();
+//     };
+//obj.sum();
+
+// function User (name, id){
+//     this.name = name;
+//     this.id =id;
+//     this.human = true;
+// }
+// let ivan = new User('Ivan', 23);
+
+
+// function sayName (surname) {
+//     console.log (this);
+//     console.log (this.name+surname);
+// }
+// const user = {
+//     name :'John'
+// };
+
+// sayName.call(user,'Smith');
+// sayName.apply(user, ['Smith']);
+/*
+function count(num){
+    return this*num;
+}
+const double = count.bind(2);
+console.log(double(3));
+console.log(double(13));
+*/
+'use strict';
+const btn = document.querySelector('button');
+btn.addEventListener('click', () => {
+    // e.target.style.backgroundColor='red';
+    this.style.backgroundColor='green';
+    // console.log(this);
+    // this.style.backgroundColor = 'red';
+    
+});
+
+// const obj = {
+//     num : 5,
+//     sayNumber : function(){
+//         const say = function(){
+//             console.log(this.num);
+//         }
+//         say();
+//     }
+
+// };
+// obj.sayNumber();
+
+// const doble = a => a*2;
+// console.log(doble(4));
+

7c257773514f274ff187bfae9ef83ca4ed006a90
yurik-007 <yurik-007@mail.ru>
2022-02-24 14:20:33 +0300

    sort obj

--- js/script.js
+++ js/script.js
@@ -35,11 +35,11 @@
 //     console.log('finally');
 // });
 
-const test = time =>{
-    return new Promise(resolve =>{
-        setTimeout(() =>resolve(), time);
-    }); 
-};
+// const test = time =>{
+//     return new Promise(resolve =>{
+//         setTimeout(() =>resolve(), time);
+//     }); 
+// };
 
 // test(1000).then(() =>{
 //     console.log('1000 ms');
@@ -52,6 +52,60 @@ const test = time =>{
 //     console.log('All');
 // })
 
-Promise.race([test(1000),test(2000)]).then(() => {
-    console.log('All');
-});
+// Promise.race([test(1000),test(2000)]).then(() => {
+//     console.log('All');
+// });
+
+
+// методы перебора массивов
+/*
+//Filter
+
+
+const names = ['Ivan', 'Ann', 'Ksenya', 'Voldemar'],
+    shortNames=names.filter(function(name){
+        return name.length <5;
+    });
+
+console.log(shortNames);
+
+//map
+
+let answers=['IvAn','AnnA', 'HellO'],
+
+    result = answers.map(item => item.toLowerCase());
+console.log(result);
+
+//every/some()
+
+const arr=[4, 'fsdfs', 'sdfsdf'],
+arr1=[4, 5, '8'];
+
+console.log(arr.some(item => typeof(item)==='number'));
+
+console.log(arr1.every(item => typeof(item)==='number')); 
+*/
+
+// reduce
+
+const arr = [4, 5, 1, 3, 2, 6];
+
+const res=arr.reduce((sum, current)=> sum+current,3);
+console.log(res);
+
+
+// const arr = ['apple','pear','plum'];
+
+// const res=arr.reduce((sum, current)=> `${sum}, ${current}`);
+// console.log(res);
+
+const obj = {
+    ivan: 'person',
+    ann: 'person',
+    dog: 'animal',
+    cat: 'animal'
+};
+const newArr=Object.entries(obj)
+.filter(item => item[1]==='person')
+.map(item => item[0]);
+console.log(newArr);

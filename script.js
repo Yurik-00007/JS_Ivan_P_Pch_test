79e624533d2957b41e314063c782b56f46628d75
yurik-007 <yurik-007@mail.ru>
2022-03-12 07:02:16 +0300

    065 регулярные выражения

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 'use strict';
+
+/*
+
 // localStorage.setItem('number', 5);
 // localStorage.removeItem('number');
 // // localStorage.clear();
@@ -45,3 +48,27 @@ checkbox.addEventListener('change', () => {
         localStorage.setItem('alex', serializedPersone);
 
         console.log(JSON.parse(localStorage.getItem('alex')));
+        
+        */
+
+        // new RegExp('pattern','flags');
+        // /pattern/flags
+
+        // const ans = prompt('Введите ваше имя');
+        // const reg = / /ig;
+        // console.log(reg.test(ans));
+
+        // const reg = /\d/g;
+        // console.log(ans.match(reg).join('')+'px');
+
+        // console.log(ans.search(reg));
+        // console.log(ans.match(reg));
+
+        // const pass = prompt ('Password');
+
+        // console.log(pass.replace(/\./g, '*'));
+
+        // console.log('12-34-56'.replace(/-/g,':'));
+        
+        const str = 'My name is R2D2';
+        console.log(str.match(/\D/g));

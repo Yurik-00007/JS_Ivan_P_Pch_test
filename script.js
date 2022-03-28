c35195fdf39326f8d230ffa1c4975e142444342c
yurik-007 <yurik-007@mail.ru>
2022-03-28 14:48:53 +0300

    finish

--- script.js
+++ script.js
@@ -51,24 +51,82 @@ checkbox.addEventListener('change', () => {
         
         */
 
-        // new RegExp('pattern','flags');
-        // /pattern/flags
+// new RegExp('pattern','flags');
+// /pattern/flags
 
-        // const ans = prompt('Введите ваше имя');
-        // const reg = / /ig;
-        // console.log(reg.test(ans));
+// const ans = prompt('Введите ваше имя');
+// const reg = / /ig;
+// console.log(reg.test(ans));
 
-        // const reg = /\d/g;
-        // console.log(ans.match(reg).join('')+'px');
+// const reg = /\d/g;
+// console.log(ans.match(reg).join('')+'px');
 
-        // console.log(ans.search(reg));
-        // console.log(ans.match(reg));
+// console.log(ans.search(reg));
+// console.log(ans.match(reg));
 
-        // const pass = prompt ('Password');
+// const pass = prompt ('Password');
 
-        // console.log(pass.replace(/\./g, '*'));
+// console.log(pass.replace(/\./g, '*'));
 
-        // console.log('12-34-56'.replace(/-/g,':'));
-        
-        const str = 'My name is R2D2';
-        console.log(str.match(/\D/g));
+// console.log('12-34-56'.replace(/-/g,':'));
+
+// const str = 'My name is R2D2';
+// console.log(str.match(/\D/g));
+
+//Promise
+
+let a = 7;
+
+// setTimeout(() => {
+//     a=99;
+//     console.log(a);
+// },2000);
+
+// console.log(a);
+
+// let b=new Promise(function(resolve,reject){
+//     setTimeout(() => {
+//         resolve(a=99);
+
+//     },2000);
+// });
+// b.then(function(){
+//     console.log(a);
+// });
+
+'use strict';
+
+console.log('Запрос данных...');
+
+const req = new Promise(function (resolve, reject) {
+    setTimeout(e => {
+
+        console.log('Подготовка данных...');
+        const product = {
+            name: 'TV',
+            price: 2000
+        };
+
+        resolve(product);
+
+    }, 2000);
+});
+req.then(product => {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            product.status = 'order';
+            resolve(product);
+            // reject();
+        }, 2000);
+    });
+}).then(data => {
+    data.modify = true;
+    return data;
+
+}).then(data => {
+    console.log(data);
+}).catch(() =>{
+    console.log('Произошла ошибка');
+}).finally(()=>{
+    console.log('finally');
+});

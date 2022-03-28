8fd1f6d83c8f6df590b37d9db95560fe9fadc323
yurik-007 <yurik-007@mail.ru>
2022-03-28 14:50:32 +0300

    Свойства акцессоры геттеры и сеттеры

--- script.js
+++ script.js
@@ -1,74 +1,14 @@
-'use strict';
+const person = {
+	name: 'Alex',
+	age: 26,
 
-/*
-
-// localStorage.setItem('number', 5);
-// localStorage.removeItem('number');
-// // localStorage.clear();
-// console.log(localStorage.getItem('number'));
-
-
-const checkbox = document.querySelector('#checkbox'),
-    form = document.querySelector('form'),
-    change = document.querySelector('#color');
-
-if (localStorage.getItem('isChecked')) {
-    checkbox.checked = true;
-}
-if (localStorage.getItem('bg') === 'changed') {
-    form.style.backgroundColor = 'red';
-}
-
-checkbox.addEventListener('change', () => {
-            if (localStorage.getItem('isChecked')) {
-                localStorage.removeItem('isChecked');
-                //   localStorage.setItem('isCheced', true);
-            } else {
-                localStorage.setItem('isChecked', true);
-
-            }
-        });
-
-        change.addEventListener('click', () => {
-            if (localStorage.getItem('bg')) {
-                localStorage.removeItem('bg');
-                form.style.backgroundColor = 'white';
-            } else {
-                localStorage.setItem('bg', 'changed');
-                form.style.backgroundColor = 'red';
-            }
-        });
-
-        const persone = {
-            name:'Alex',
-            age: 25
-        };
-
-        const serializedPersone=JSON.stringify(persone);
-        localStorage.setItem('alex', serializedPersone);
-
-        console.log(JSON.parse(localStorage.getItem('alex')));
-        
-        */
-
-        // new RegExp('pattern','flags');
-        // /pattern/flags
-
-        // const ans = prompt('Введите ваше имя');
-        // const reg = / /ig;
-        // console.log(reg.test(ans));
-
-        // const reg = /\d/g;
-        // console.log(ans.match(reg).join('')+'px');
-
-        // console.log(ans.search(reg));
-        // console.log(ans.match(reg));
-
-        // const pass = prompt ('Password');
-
-        // console.log(pass.replace(/\./g, '*'));
-
-        // console.log('12-34-56'.replace(/-/g,':'));
-        
-        const str = 'My name is R2D2';
-        console.log(str.match(/\D/g));
+	get userAge() {
+		return this.age;
+	},
+	set userAge(num) {
+		this.age = num;
+	}
+};
+console.log(person.userAge);
+console.log(person.userAge = 30);
+console.log(person.userAge);
